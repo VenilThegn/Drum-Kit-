c2d5dfe1ddcb47474d948460b10447d73b8a919a
VenilThegn <89027206+VenilThegn@users.noreply.github.com>
2022-02-12 19:17:57 +0530

    colour change and keydown keys

--- kittle.js
+++ kittle.js
@@ -1,71 +1,77 @@
-for ( var i = 0; i < document.querySelectorAll(".alpha").length; i++)
+document.querySelector(".instruction").addEventListener("click", function() {
+  alert("Blue means Right alphabet is right place.     yelow means right alphabet in wrong place.  No change means wrong alphabet. ")
+})
+for (var i = 0; i < document.querySelectorAll(".alpha").length; i++)
 
-{var k =0; var j=1; var ent=".try"; var entry= ent+j; var res;
- var right="siare";
+{
+  var k = 0;
+  var j = 1;
+  var res;
+  var right = "siare";
   document.querySelectorAll(".alpha")[i].addEventListener("click", function() {
-    var pressed = this.innerHTML;
-
-
-
-if(pressed==="del")
-{k--;
-  document.querySelectorAll(entry)[k].innerHTML=" ";
-
-
-}
-else if (pressed==="enter")
-{
-  if(j<=5)
-{ res=colour(right);
-  if(res==5)
-  {
-    alert("you won")
-  }
-
-  j++;
-  entry=ent+j;
-  k=0;
-  console.log(entry);
-  if((j==6)&&(res!=5))
-  {
-    alert("game over . The right answer is "+right);
-  }
-}
-else
-{
-    alert("game over . The right answer is "+right);
-}
-}
-else{
-  document.querySelectorAll(".try"+j)[k].innerHTML=pressed;
-  k++;
-}
-
+    var press = this.innerHTML;
+    check(press)
   });
+}
+
+window.addEventListener('keydown', function(eve) {
+
+console.log(eve);
+check(eve.key);
+
+
+});
+
+
+
+
+function check(pressed) {
+  if (pressed == "Backspace") {
+    k--;
+    document.querySelectorAll(".try"+j)[k].innerHTML = " ";
+
+
+  } else if (pressed === "Enter") {
+    if (j <= 5) {
+      res = colour(right);
+      if (res == 5) {
+        alert("you won")
+      }
+      j++;
+      k = 0;
+      console.log(".try"+j);
+      if ((j == 6) && (res != 5)) {
+        alert("game over . The right answer is " + right);
+      }
+    } else {
+      alert("game over . The right answer is " + right);
+    }
+  } else {
+    document.querySelectorAll(".try" + j)[k].innerHTML = pressed;
+    k++;
+  }
 
 }
-function colour(correct)
-{ var temp=0;
 
-  for(var x =0;x<5;x++)
-  {
-    for(var y =0;y<5;y++)
-    {
-      if((correct[x]==document.querySelectorAll(".try"+j)[y].innerHTML)&&(x==y))
-      { temp++;
-        document.querySelectorAll(".try"+j)[y].classList.add("blue");
-        document.querySelector("."+correct[x]).classList.add("blue");
-        document.querySelector("."+correct[x]).classList.remove("yellow");
+
+function colour(correct) {
+  var temp = 0;
+
+  for (var x = 0; x < 5; x++) {
+    for (var y = 0; y < 5; y++) {
+      if ((correct[x] == document.querySelectorAll(".try" + j)[y].innerHTML) && (x == y)) {
+        temp++;
+        document.querySelectorAll(".try" + j)[y].classList.add("blue");
+        document.querySelector("." + correct[x]).classList.add("blue");
+        document.querySelector("." + correct[x]).classList.remove("yellow");
+
+
+      } else if ((correct[x] == document.querySelectorAll(".try" + j)[y].innerHTML) && (x != y)) {
+        document.querySelectorAll(".try" + j)[y].classList.add("yellow");
+        document.querySelector("." + correct[x]).classList.add("yellow");
+
       }
-      else if ((correct[x]==document.querySelectorAll(".try"+j)[y].innerHTML)&&(x!=y))
-      {
-        document.querySelectorAll(".try"+j)[y].classList.add("yellow");
-          document.querySelector("."+correct[x]).classList.add("yellow");
-      }
-      else
-      {
-        document.querySelectorAll(".try"+j)[y].classList.add("grey");
-      }
+
     }
   }
   return temp;
