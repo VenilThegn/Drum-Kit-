92d85214d08b6053e1a8c673b6a1a67aaf8e8848
VenilThegn <89027206+VenilThegn@users.noreply.github.com>
2022-02-19 00:18:18 +0530

    added more words(52)

--- kittle.js
+++ kittle.js
@@ -1,12 +1,12 @@
-
 document.querySelector(".instruction").addEventListener("click", function() {
   alert("Colour changes in the alphabets after entering a word indicates:\nGreen->The alphabet you have entered is correct and is in the right position.\nYellow->The alphabet you have entered is correct but not in the right position\nNo change-> The alphabet is not present in the word.");
 })
-var words = new Array("abode", "chase", "cabin", "satin", "light", "flake", "quail");
-var n =Math.floor(Math.random()*7);
-// might, white, brave, crave, dream, steam, stare, flare, great, break, brake, trace, satin, haste, waste, eight, flake, poise, noise, voice, vocal, focal, focus, train, crate, sedan, paste, paint, faint, whale, shale, frail, grail, chain, quail, image, zebra, yacht, adopt, urban, eland, extra, hover, cover, lover
-var right =words[n];
+var words = new Array("might", "white", "abode", "chase", "cabin", "satin", "light", "flake", "quail", "brave", "crave", "dream", "steam", "stare", "flare", "great", "break", "brake", "trace", "satin", "haste", "waste", "eight", "flake", "poise", "noise", "voice", "vocal", "focal", "focus", "train", "crate", "sedan", "paste", "paint", "faint", "whale", "shale", "frail", "grail", "chain", "quail", "image", "zebra", "yacht", "adopt", "urban", "eland", "extra", "hover", "cover", "lover");
+var n = Math.floor(Math.random() * 52);
 
+
+var right = words[n];
+// console.log(words[n]+" "+n);
 n++;
 
 for (var i = 0; i < document.querySelectorAll(".alpha").length; i++)
@@ -26,8 +26,8 @@ for (var i = 0; i < document.querySelectorAll(".alpha").length; i++)
 
 window.addEventListener('keydown', function(eve) {
 
-console.log(eve);
-check(eve.key);
+
+  check(eve.key);
 
 
 });
@@ -36,29 +36,33 @@ check(eve.key);
 
 
 function check(pressed) {
-  if ((pressed == "Backspace")&&(k>0)) {
+  if ((pressed == "Backspace") && (k > 0)) {
     k--;
-    document.querySelectorAll(".try"+j)[k].innerHTML = " ";
+    document.querySelectorAll(".try" + j)[k].innerHTML = " ";
 
 
-  } else if ((pressed === "Enter")&&(k==5)) {
+  } else if ((pressed === "Enter") && (k == 5)) {
     if (j <= 5) {
       res = colour(right);
       console.log(res);
       if (res == 5) {
-        alert("congratulations you won in "+j+"/5 chances."+" You have 7 words to find. Keep refreshing ");
-        setTimeout(function(){window.location.reload()},4000);
+        alert("congratulations you won in " + j + "/5 chances." + " Keep refreshing for different words ");
+        setTimeout(function() {
+          window.location.reload()
+        }, 4000);
       }
       j++;
       k = 0;
-      console.log(".try"+j);
+      console.log(".try" + j);
       if ((j == 6) && (res != 5)) {
         alert("game over . The right answer is " + right);
-        setTimeout(function(){window.location.reload()},1000);
+        setTimeout(function() {
+          window.location.reload()
+        }, 1000);
       }
 
     }
-  } else if((pressed>="a")&&(pressed<="z")) {
+  } else if ((pressed >= "a") && (pressed <= "z")) {
     document.querySelectorAll(".try" + j)[k].innerHTML = pressed;
     k++;
   }
